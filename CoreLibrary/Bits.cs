843afc2a8bb97accd4fa54d11a905905d34cfccd
iAmJoker94 <54372495+iAmJoker94@users.noreply.github.com>
2019-08-26 20:20:37 +0100

    ...

--- CoreLibrary/Bits.cs
+++ CoreLibrary/Bits.cs
@@ -12,5 +12,20 @@ namespace JokerLib
         {
             return (value & 0x000000FFU) << 24 | (value & 0x0000FF00U) << 8 | (value & 0x00FF0000U) >> 8 | (value & 0xFF000000U) >> 24;
         }
+
+        public static byte[] ReverseArrayByDword(byte[] value)
+        {
+            if (value.Length % 4 != 0)
+                throw new Exception("Invalid length");
+            var builder = new List<byte>();
+            for (int i = 0; i < value.Length - 4; i += 4)
+            {
+                var take = new byte[] { value[i], value[i + 1], value[i + 2], value[i + 3] };
+                Array.Reverse(take);
+                foreach (var c in take)
+                    builder.Add(c);
+            }
+            return builder.ToArray();
+        }
     }
 }
